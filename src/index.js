f5e9edca76a087abea0a500f815767319c521397
gruzam0615 <gruzam0615@gmail.com>
2024-05-27 02:12:49 +0900

    signIn Action Testing complete

--- src/index.js
+++ src/index.js
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import SignIn from './pages/SignIn';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/sign",
+    element: <SignIn />,
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <RecoilRoot>
+    <RouterProvider router={router} />
+  </RecoilRoot>
   </React.StrictMode>
 );
 
